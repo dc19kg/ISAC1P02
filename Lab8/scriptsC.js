f9ddddce9881f6783d7253679db4f9360fcccdb6
dc19kg <dc19kg@brocku.ca>
2021-11-04 14:50:44 -0400

    Lab 8

--- Lab8/scriptsC.js
+++ Lab8/scriptsC.js
@@ -1,6 +1,6 @@
-function addNum(){
-  document.getElementById('zero').innerHTML="0";
-  document.getElementById('one').innerHTML="1";
-  document.getElementById('two').innerHTML="2";
-  document.getElementById('three').innerHTML="3";
+document.write("Output Zero Here");
+
+//This will output when a button is pressed
+function theRest(){
+	document.getElementById('output3').innerHTML="Output Three Here";
 }
